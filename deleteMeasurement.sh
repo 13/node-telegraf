6ddf24cd034d47a609c9174bae3c24eb8049f9b4
nofe4rz@gmail.com <nofe4rz@gmail.com>
2023-11-05 13:15:13 +0100

    added measurement deletion script if wrong datatype int float

---
+++ deleteMeasurement.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+# Delete measurement ie wrong datatype float int
+
+#
+TOKEN=$(grep -o "influxToken *= *process.env\['INFLUX_TOKEN'\] *|| *'[^']*'" "env.mjs" | awk -F"'" '{print $4}')
+
+influx delete --bucket muh --org muh \
+  --token $TOKEN \
+  --start '1970-01-01T00:00:00Z' \
+  --stop $(date +"%Y-%m-%dT%H:%M:%SZ") \
+  --predicate '_measurement="wind_gust_bft"'
+
+# --predicate '_measurement="example-measurement" AND exampleTag="exampleTagValue"'
